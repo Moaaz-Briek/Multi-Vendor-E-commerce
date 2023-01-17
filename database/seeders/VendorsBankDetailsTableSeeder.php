89eb5eb3e92a45181aa5af026e05c67e8d93609f
moaaz97 <moaaz.briek97@gmail.com>
2023-01-17 22:09:58 +0200

    Manage Vendor Details

---
+++ database/seeders/VendorsBankDetailsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\VendorsBankDetail;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+class VendorsBankDetailsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $vendorRecords = [
+            'vendor_id' => 1,
+            'account_holder_name' => 'Amen Briek',
+            'account_number' => '388961416185611655561',
+            'bank_name' => 'Ahly-bank',
+            'bank_ifsc_code' => '454545',
+        ];
+        VendorsBankDetail::insert($vendorRecords);
+    }
+}
---
+++ database/seeders/VendorsBusinessDetailsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\VendorsBusinessDetail;
+class VendorsBusinessDetailsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $vendorRecords = [
+          'id' => 1,
+          'vendor_id' => 1,
+          'shop_name' => 'Amen Yarn Store',
+            'shop_address' => 'Younes-streat',
+            'shop_city' => 'shebien',
+            'shop_state' => 'qaluibia',
+            'shop_country' => 'egypt',
+            'shop_pincode' => '13762',
+            'shop_mobile' => '0101173520',
+            'shop_website' => 'Amen-Yarn.com',
+            'shop_email' => 'Amen@briek.com',
+            'address_prof' => 'passport',
+            'address_prof_image' => 'test.jpg',
+            'business_license_number' => '123456789',
+        ];
+        VendorsBusinessDetail::insert($vendorRecords);
+    }
+}
