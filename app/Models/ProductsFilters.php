3e8069cd9e80a721831267c7ef45e0db0e005b47
moaaz97 <moaaz.briek97@gmail.com>
2023-04-04 07:08:19 +0200

    create getFilterName method

--- app/Models/ProductsFilters.php
+++ app/Models/ProductsFilters.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductsFilters extends Model
 {
     use HasFactory;
+
+    public static function getFilterName($filter_id)
+    {
+        $filter = ProductsFilters::select('filter_name')->where('id', $filter_id)->first();
+        return $filter->filter_name;
+    }
 }
